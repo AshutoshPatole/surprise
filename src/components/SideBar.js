c2eb14e6ac117fbc8f24913bbe6e365f7f2b636a
AshutoshPatole <apatole479@gmail.com>
2023-04-09 13:41:29 +0530

    Fix: sidebar collide with appbar

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -3,10 +3,10 @@ import { Link } from "react-router-dom";
 import "./SideBar.css";
 
 const levels = [
-  { level: 0, name: "Bandit Level 0", link: "/war/0" },
-  { level: 1, name: "Bandit Level 1", link: "/war/1" },
-  { level: 2, name: "Bandit Level 2", link: "/war/2" },
-  { level: 3, name: "Bandit Level 3", link: "/war/3" },
+  { level: 0, name: "War Level 0", link: "/war/0" },
+  { level: 1, name: "War Level 1", link: "/war/1" },
+  { level: 2, name: "War Level 2", link: "/war/2" },
+  { level: 3, name: "War Level 3", link: "/war/3" },
   // add more levels here
 ];
 
--- src/containers/WarGame/WarGame.js
+++ src/containers/WarGame/WarGame.js
@@ -1,15 +1,18 @@
 import MyAppBar from "../../components/AppBar";
 import Sidebar from "../../components/SideBar";
 import { getUserDetails } from "../../utils/user_details";
+import "./WarGame.css";
 const WarGame = () => {
   const u = getUserDetails();
   return (
     <div>
-      <Sidebar></Sidebar>
-
       <MyAppBar></MyAppBar>
-      <div className="info">
-        <h1>Hi, {u.userName}</h1>
+      <div className="levels">
+        <Sidebar></Sidebar>
+
+        <div className="levels-info">
+          <h1>Hi, {u.userName}</h1>
+        </div>
       </div>
     </div>
   );
